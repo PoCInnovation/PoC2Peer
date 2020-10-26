8b45e186e49acb2f1329172f4fdabf6fb1900562
BERNARD Damien <damien.bernard@epitech.eu>
2020-10-26 16:40:06 +0100

    add (project): Adding Torrentfiles structure.

---
+++ GoBittorent/torrentfile/torrentfile.go
@@ -0,0 +1,52 @@
+package torrentfile
+
+import (
+	"bytes"
+	"crypto/rand"
+	"crypto/sha1"
+	"fmt"
+	"os"
+
+	"github.com/jackpal/bencode-go"
+	"github.com/veggiedefender/torrent-client/p2p"
+)
+
+// Port to listen on
+const Port uint16 = 6881
+
+// TorrentFile encodes the metadata from a .torrent file
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+type bencodeInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+// DownloadToFile downloads a torrent and writes it to a file
+func (t *TorrentFile) DownloadToFile(path string) error {
+	var peerID [20]byte
+	_, err := rand.Read(peerID[:])
+	if err != nil {
+		return err
+	}
+
+	peers, err := t.requestPeers(peerID, Port)
+	if err != nil {
+		return err
+	}
+
+}
