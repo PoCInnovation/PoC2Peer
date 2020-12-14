d7e536e196ddbfed6ff9f1979c19d85bd6246854
BERNARD Damien <damien.bernard@epitech.eu>
2020-12-14 16:37:16 +0100

    update (GoLib): Reworking server with new bootstrap and protocol updated.

--- GoRework/Protocol/main.go
+++ GoRework/Protocol/main.go
@@ -1,76 +1,157 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"crypto/rand"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	mrand "math/rand"
 
 	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
-	mplex "github.com/libp2p/go-libp2p-mplex"
-	secio "github.com/libp2p/go-libp2p-secio"
-	yamux "github.com/libp2p/go-libp2p-yamux"
-	tcp "github.com/libp2p/go-tcp-transport"
-	ws "github.com/libp2p/go-ws-transport"
-	"github.com/multiformats/go-multiaddr"
+
+	ds "github.com/ipfs/go-datastore"
+	dsync "github.com/ipfs/go-datastore/sync"
+	golog "github.com/ipfs/go-log"
+
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	rhost "github.com/libp2p/go-libp2p/p2p/host/routed"
+	ma "github.com/multiformats/go-multiaddr"
+	gologging "github.com/whyrusleeping/go-logging"
 )
 
+func makeRoutedHost(listenPort int, randseed int64, bootstrapPeers []peer.AddrInfo, globalFlag string) (host.Host, error) {
+
+	var r io.Reader
+	if randseed == 0 {
+		r = rand.Reader
+	} else {
+		r = mrand.New(mrand.NewSource(randseed))
+	}
+
+	priv, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, r)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listenPort)),
+		libp2p.Identity(priv),
+		libp2p.DefaultTransports,
+		libp2p.DefaultMuxers,
+		libp2p.DefaultSecurity,
+		libp2p.NATPortMap(),
+	}
+	ctx := context.Background()
+	basicHost, err := libp2p.New(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+	dstore := dsync.MutexWrap(ds.NewMapDatastore())
+	dht := dht.NewDHT(ctx, basicHost, dstore)
+	routedHost := rhost.Wrap(basicHost, dht)
+	err = bootstrapConnect(ctx, routedHost, bootstrapPeers)
+	if err != nil {
+		return nil, err
+	}
+	err = dht.Bootstrap(ctx)
+	if err != nil {
+		return nil, err
+	}
+	hostAddr, _ := ma.NewMultiaddr(fmt.Sprintf("/ipfs/%s", routedHost.ID().Pretty()))
+	addrs := routedHost.Addrs()
+	log.Println("I can be reached at:")
+	for _, addr := range addrs {
+		log.Println(addr.Encapsulate(hostAddr))
+	}
+	log.Printf("Now run \"./routed-echo -l %d -d %s%s\" on a different terminal\n", listenPort+1, routedHost.ID().Pretty(), globalFlag)
+	return routedHost, nil
+}
+
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	golog.SetAllLoggers(golog.LogLevel(gologging.INFO)) // Change to DEBUG for extra info
+	listenF := flag.Int("l", 0, "wait for incoming connections")
+	target := flag.String("d", "", "target peer to dial")
+	seed := flag.Int64("seed", 0, "set random seed for id generation")
+	global := flag.Bool("global", false, "use global ipfs peers for bootstrapping")
+	flag.Parse()
 
-	transports := libp2p.ChainOptions(
-		libp2p.Transport(tcp.NewTCPTransport),
-		libp2p.Transport(ws.New),
-	)
+	if *listenF == 0 {
+		log.Fatal("Please provide a port to bind on with -l")
+	}
 
-	muxers := libp2p.ChainOptions(
-		libp2p.Muxer("/yamux/1.0.0", yamux.DefaultTransport),
-		libp2p.Muxer("/mplex/6.7.0", mplex.DefaultTransport),
-	)
-
-	security := libp2p.Security(secio.ID, secio.New)
-
-	listenAddrs := libp2p.ListenAddrStrings(
-		"/ip4/0.0.0.0/tcp/0",
-		"/ip4/0.0.0.0/tcp/0/ws",
-	)
-
-	host, err := libp2p.New(
-		ctx,
-		transports,
-		listenAddrs,
-		muxers,
-		security,
-	)
+	var bootstrapPeers []peer.AddrInfo
+	var globalFlag string
+	if *global {
+		log.Println("using global bootstrap")
+		bootstrapPeers = IPFS_PEERS
+		globalFlag = " -global"
+	} else {
+		log.Println("using local bootstrap")
+		bootstrapPeers = getLocalPeerInfo()
+		globalFlag = ""
+	}
+	ha, err := makeRoutedHost(*listenF, *seed, bootstrapPeers, globalFlag)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	// TODO: Register a stream handler for our protocol!
+	ha.SetStreamHandler("/echo/1.0.0", func(s network.Stream) {
+		log.Println("Got a new stream!")
+		if err := doEcho(s); err != nil {
+			log.Println(err)
+			s.Reset()
+		} else {
+			s.Close()
+		}
+	})
 
-	for _, addr := range host.Addrs() {
-		fmt.Println("Listening on", addr)
+	if *target == "" {
+		log.Println("listening for connections")
+		select {} // hang forever
 	}
 
-	targetAddr, err := multiaddr.NewMultiaddr("/ip4/127.0.0.1/tcp/63785/p2p/QmWjz6xb8v9K4KnYEwP5Yk75k5mMBCehzWFLCvvQpYxF3d")
+	peerid, err := peer.IDB58Decode(*target)
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	targetInfo, err := peer.AddrInfoFromP2pAddr(targetAddr)
+	// peerinfo := peer.AddrInfo{ID: peerid}
+	log.Println("opening stream")
+	s, err := ha.NewStream(context.Background(), peerid, "/echo/1.0.0")
+
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	err = host.Connect(ctx, *targetInfo)
+	_, err = s.Write([]byte("Hello, world!\n"))
 	if err != nil {
-		panic(err)
+		log.Fatalln(err)
 	}
 
-	fmt.Println("Connected to", targetInfo.ID)
+	out, err := ioutil.ReadAll(s)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	// TODO: Implement a simple read loop to read user input and send messages
-	// to peers we are connected to that support our protocol.
+	log.Printf("read reply: %q\n", out)
+}
 
-	host.Close()
+func doEcho(s network.Stream) error {
+	buf := bufio.NewReader(s)
+	str, err := buf.ReadString('\n')
+	if err != nil {
+		return err
+	}
+
+	log.Printf("read: %s\n", str)
+	_, err = s.Write([]byte(str))
+	return err
 }
