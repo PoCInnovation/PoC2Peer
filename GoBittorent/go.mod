ef70db725589b20c61d9267e3b341f19c75b145b
BERNARD Damien <damien.bernard@epitech.eu>
2021-02-05 14:49:57 +0100

    Delete (GoLib): Deleting all old examples of codes and past work on torrent protocol (good enough comprehension now).

--- GoBittorent/go.mod
+++
@@ -1,8 +0,0 @@
-module Poc2PeerBitTorrent
-
-go 1.15
-
-require (
-	github.com/jackpal/bencode-go v1.0.0
-	github.com/stretchr/testify v1.6.1
-)
---
+++ Poc2PeerLibrary/gomobile/MobileInterface.go
@@ -0,0 +1,62 @@
+package gomobile
+
+import (
+	"fmt"
+	"time"
+	"github.com/PoCInnovation/PoC2Peer/Poc2PeerLibrary/core"
+)
+
+var Lib core.LibP2pCore
+
+// callback
+var jc JavaCallback
+
+type JavaCallback interface {
+	SendString(string)
+}
+
+type TestStruct struct {
+	Str string
+}
+
+var packageVar = TestStruct{Str: "InsidePackagevar"}
+
+func RegisterJavaCallback(c JavaCallback) {
+	jc = c
+}
+
+func TestCall() {
+	for i := 0; i < 100; i++ {
+		time.Sleep(100 * time.Millisecond)
+		jc.SendString(fmt.Sprintln("Counting... ", i))
+	}
+}
+
+func CallString() string {
+	return "string -> Maxime, je suis le meilleur prends moi chez Skillz STP\n"
+}
+
+func CallByteArray() []byte {
+	return []byte("byte array -> Maxime, je suis le meilleur prends moi chez Skillz STP\n")
+}
+
+func CallIntArray() []uint {
+	return []uint{1, 2, 3}
+}
+
+func CallInterfaceInt() interface{} {
+	return []int{1, 2, 3}
+}
+
+func CallMap() map[string]int {
+	return map[string]int{"lol": 1, "mdr":2, "xd": 3}
+}
+
+func CallInterfaceMap() interface{} {
+	return map[string]int{"lol": 1, "mdr":2, "xd": 3}
+}
+
+func CallPackageVariableStringified() string {
+	return fmt.Sprintf("%v\n", packageVar)
+}
+
