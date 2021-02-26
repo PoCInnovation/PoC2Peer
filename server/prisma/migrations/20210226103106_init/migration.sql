f6e94479cc8e61fd26f9958ac67a92e71edd48e2
Gregoire Brasseur <gregoirebrasseurpro@gmail.com>
2021-02-26 11:45:13 +0100

    add: prisma database

---
+++ server/prisma/migrations/20210226103106_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "album" VARCHAR(255) NOT NULL,
+    "autor" VARCHAR(255) NOT NULL,
+    "genre" VARCHAR(255) NOT NULL,
+    "source" VARCHAR(255) NOT NULL,
+    "image" VARCHAR(255) NOT NULL,
+    "trackNumber" INTEGER NOT NULL,
+    "totalTrackCount" INTEGER NOT NULL,
+    "duration" INTEGER NOT NULL,
+    "site" VARCHAR(255) NOT NULL,
+
+    PRIMARY KEY ("id")
+);
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,18 +2,49 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import httpStatus from 'http-status-codes';
+import { PrismaClient } from '@prisma/client';
 
 const PORT = 3000;
 const server = express();
+const prisma = new PrismaClient();
 
 server.use(bodyParser.json());
 server.use(cookieParser());
 
 server.get('/health', (req, res) => {
-  res.sendStatus(httpStatus.OK);
+  prisma.post.findMany().then((data) => {
+    res.status(httpStatus.OK).send(data);
+  });
+  // console.log(allPost);
 });
 
+async function main() {
+  await prisma.post.create({
+    data: {
+      title: 'Intro - The Way Of Waking Up (feat. Alan Watts)',
+      album: 'Wake Up',
+      autor: 'The Kyoto Connection',
+      genre: 'Electronic',
+      source: 'https://storage.googleapis.com/uamp/The_Kyoto_Connection_-_Wake_Up/01_-_Intro_-_The_Way_Of_Waking_Up_feat_Alan_Watts.mp3',
+      image: 'https://storage.googleapis.com/uamp/The_Kyoto_Connection_-_Wake_Up/art.jpg',
+      trackNumber: 1,
+      totalTrackCount: 13,
+      duration: 90,
+      site: 'http://freemusicarchive.org/music/The_Kyoto_Connection/Wake_Up_1957/',
+    },
+  });
+  const allPost = await prisma.post.findMany();
+  console.log(allPost);
+}
+
 server.listen(PORT, () => {
+  main()
+    .catch((e) => {
+      throw e;
+    })
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
   console.log(`server is listening on ${PORT}`);
 });
 
