c04ed00200821f0a2c7d903a46bc1e0a263872d7
Gregoire Brasseur <gregoirebrasseurpro@gmail.com>
2021-03-11 21:18:31 +0100

    add: add peer function

--- dashboard/lib/List_action.dart
+++ dashboard/lib/List_action.dart
@@ -1,21 +1,100 @@
 import 'dart:developer';
 
+import 'package:dashboard/Api_request.dart';
 import 'package:flutter/material.dart';
 import 'tracktype.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'Card.dart';
 
+popPeerDialog(BuildContext context) {
+  final idpeercontroler = TextEditingController();
+  final ippeercontroler = TextEditingController();
+
+  return showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (_) => AlertDialog(
+      title: Text('Add Peer'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            TextField(
+              controller: idpeercontroler,
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'IdPeer',
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'IpPeer',
+              ),
+              controller: ippeercontroler,
+            )
+          ],
+        ),
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: Text('Add'),
+          onPressed: () {
+            addPeer(idpeercontroler.text, ippeercontroler.text);
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ),
+  );
+}
+
 class ActionList extends StatelessWidget {
-  const ActionList({Key key}) : super(key: key);
+  final Function reloadPeer;
+  final Function reloadTrack;
+  ActionList({Key key, this.reloadPeer, this.reloadTrack}) : super(key: key);
+
+  bool addpeer = false;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        ElevatedButton(
-          onPressed: () => print('hello'),
-          child: Text('hello'),
+        Container(
+          height: 30,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () => initDB(),
+            child: Text('Init'),
+          ),
+        ),
+        Container(height: 5),
+        Container(
+          height: 30,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () => reloadPeer(),
+            child: Text('fetchPeer'),
+          ),
+        ),
+        Container(height: 5),
+        Container(
+          height: 30,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () => reloadTrack(),
+            child: Text('fetchTrack'),
+          ),
+        ),
+        Container(height: 5),
+        Container(
+          height: 30,
+          width: 100,
+          child: ElevatedButton(
+            onPressed: () => popPeerDialog(context),
+            child: Text('AddPeer'),
+          ),
         ),
       ],
     );
